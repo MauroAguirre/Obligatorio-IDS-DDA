063e48e6ef6f2d40d09b7b8609961cf8c377617c
MauroAguirre <42445651+MauroAguirre@users.noreply.github.com>
2018-11-23 19:44:05 -0300

    creamos el waiting panel

---
+++ Client/src/main/java/uy/edu/cei/Obligatorio/Client/ui/Panel/WaitingPanel.java
@@ -0,0 +1,14 @@
+package uy.edu.cei.Obligatorio.Client.ui.Panel;
+
+import javax.swing.JPanel;
+
+public class WaitingPanel extends GeneralaPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public WaitingPanel() {
+
+	}
+
+}
